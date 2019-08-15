57622443cdf14f1d4b4404877ca067b7827ec943
ArnaudDuthoit <43751449+ArnaudDuthoit@users.noreply.github.com>
2019-08-15 11:02:29 +0200

    Create DOM in JS
    Took 1 hour 51 minutes

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+var originalDOM = document.getElementById('contenu').innerHTML;
+
 function ajaxRequest(plateforme, player) {
+
+    document.getElementById('contenu').innerHTML = originalDOM;
 
     var xhttp = new XMLHttpRequest();
 
@@ -11,26 +15,61 @@ function ajaxRequest(plateforme, player) {
             obj = JSON.parse(myArr);
 
             if (typeof obj.errors !== 'undefined') {
-                document.getElementById('avatar').src = 'https://via.placeholder.com/40';
-                document.getElementById('name').innerHTML = obj.errors['0'].message;
-                document.getElementById('level').innerHTML = "N/A";
-                document.getElementById('country').innerHTML = "N/A";
-                document.getElementById('kills').innerHTML = "N/A";
-                document.getElementById('damage').innerHTML = "N/A";
-                document.getElementById('headshots').innerHTML = "N/A";
-                document.getElementById('revives').innerHTML = "N/A";
+
+                $('#contenu').append("<div class=\"alert alert-danger mt-4\" role=\"alert\">\n" +
+                    "  Aucun joueur trouvé " +
+                    "</div>");
+
             } else {
-                document.getElementById('avatar').src = obj.data.metadata.avatarUrl;
-                document.getElementById('name').innerHTML = obj.data.metadata.platformUserHandle;
-                document.getElementById('level').innerHTML = obj.data.metadata.level;
-                document.getElementById('country').innerHTML = obj.data.metadata.countryCode;
 
-                document.getElementById('kills').innerHTML = obj.data.stats[1].value;
-                document.getElementById('damage').innerHTML = obj.data.stats[2].value;
-                document.getElementById('headshots').innerHTML = obj.data.stats[3].value;
-                document.getElementById('revives').innerHTML = obj.data.stats[4].value;
+                $('#contenu').innerHTML ='';
 
-                Object.keys(obj).forEach(function (key) {
+                $('#player_overview').append("<div class=\"row mt-4 \">\n" +
+                    "            <table class=\"table table-striped table-dark\">\n" +
+                    "                <thead>\n" +
+                    "                <tr>\n" +
+                    "                    <th>Avatar</th>\n" +
+                    "                    <th>Username</th>\n" +
+                    "                    <th>Level</th>\n" +
+                    "                    <th>Country</th>\n" +
+                    "                </tr>\n" +
+                    "                </thead>\n" +
+                    "                <tbody>\n" +
+                    "                <tr>\n" +
+                    "                    <td><img id=\"avatar\" src="+ obj.data.metadata.avatarUrl +" width=\"40px\" height=\"40px\"></td>\n" +
+                    "                    <td id=\"name\">" + obj.data.metadata.platformUserHandle +" </td>\n" +
+                    "                    <td id=\"level\">"+ obj.data.metadata.level + "</td>\n" +
+                    "                    <td id=\"country\">"+ obj.data.metadata.countryCode+ "</td>\n" +
+                    "                </tr>\n" +
+                    "                </tbody>\n" +
+                    "            </table>\n" +
+                    "        </div>\n");
+
+
+                $('#player_overview_stats').append("<div class=\"row mt-4\">\n" +
+                    "            <table class=\"table table-striped table-dark\">\n" +
+                    "                <thead>\n" +
+                    "                <tr>\n" +
+                    "                    <th>Kills</th>\n" +
+                    "                    <th>Damage</th>\n" +
+                    "                    <th>Headshots</th>\n" +
+                    "                    <th>Revives</th>\n" +
+                    "                </tr>\n" +
+                    "                </thead>\n" +
+                    "                <tbody>\n" +
+                    "                <tr>\n" +
+                    "                    <td id=\"kills\">" + obj.data.stats[1].value +"</td>\n" +
+                    "                    <td id=\"damage\">" + obj.data.stats[2].value +"</td>\n" +
+                    "                    <td id=\"headshots\">" + obj.data.stats[3].value +"</td>\n" +
+                    "                    <td id=\"revives\">" + obj.data.stats[4].value +"</td>\n" +
+                    "                </tr>\n" +
+                    "                </tbody>\n" +
+                    "            </table>\n" +
+                    "        </div>\n");
+
+
+
+                    Object.keys(obj).forEach(function (key) {
 
                     var children = obj[key]['children'];
 
@@ -38,9 +77,42 @@ function ajaxRequest(plateforme, player) {
 
                         console.log(element);
 
-                        $('#content').append("<div class='card' id='"+element.id+"'><img class=card-img-top src='" + element.metadata.icon + "'></div>");
-                        $('#'+element.id+"").append("<div class='card-body'></div>");
-                        console.log( $('#'+element.id+""));
+                        $('#content').append("<div class='tnr-card' id='"+element.id+"' style='box-shadow: 0 1px 3px rgba(0, 0, 0, .2), 0 1px 1px rgba(0, 0, 0, .14), 0 2px 1px -1px rgba(0, 0, 0, .12);\n" +
+                            "        background: #292929;\n" +
+                            "        margin-bottom: 2.4rem;\n" +
+                            "        display: block;'></div>");
+
+                        $('#'+element.id+"").append("<div class='tnr-card_header' style='text-transform: uppercase;\n" +
+                            "    font-family: nimbus-sans-condensed,Arial,Verdana,Helvetica,sans-serif;\n" +
+                            "    font-size: 2rem;\n" +
+                            "    font-weight: 400;\n" +
+                            "    letter-spacing: .15em;\n" +
+                            "    padding: 1.2rem 1.6rem;\n" +
+                            "    margin: 0;\n" +
+                            "    color: #da292a;'>" + element.metadata.legend_name + "</div>");
+
+                        $('#'+element.id+"").append("<div class='ap-legend-stats' id='ap-legend-stats"+element.id+"' style='display: flex;\n" +
+                            "    justify-content: space-between;'></div>");
+
+                        $('#'+"ap-legend-stats"+element.id+"").append("<div class='ap-legend-stats__image' id='ap-legend-stats__image"+element.id+"'  style='width: 150px;\n" +
+                            "    background: #363636;\n" +
+                            "    min-height: 185px;\n" +
+                            "    overflow: hidden;'></div>");
+
+                        $('#'+"ap-legend-stats"+element.id+"").append("<div class='ap-legend-stats__stats' id='ap-legend-stats__stats"+element.id+"' style='flex-basis: 0;\n" +
+                            "    flex-grow: 1;\n" +
+                            "    margin: 1.6rem;'></div>");
+
+                        $('#'+"ap-legend-stats__stats"+element.id+"").append("<div class='trn-defstats-grid trn-defstats-grid--col4' id='' style='grid-gap: 2.4rem;grid-auto-flow: row;\n" +
+                            "    grid-template-columns: repeat(4,1fr);display: grid;justify-content: flex-start;'>");
+
+
+
+                        $('#'+"ap-legend-stats__image"+element.id+"").append("<div style='background-image: url("+ element.metadata.icon +");background-size: cover;\n" +
+                            "    width: 100%;\n" +
+                            "    height: 100%;'> </div>");
+
+                            console.log( $('#'+element.id+""));
                     });
 
                 });
